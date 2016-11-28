0f876896cf555d9108425f2334f4dd684207dc98
kasper storgaard <kasper.storgaard@falcon.io>
2016-11-28 20:29:50 +0100

    feat(app): route views

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,32 @@
+import {inject} from 'aurelia-framework';
+import {Router, RouterConfiguration} from 'aurelia-router';
+
+@inject(Router)
 export class App {
   public heading = 'pollr';
+  public router: Router;
+
+  public configureRouter(config: RouterConfiguration, router: Router) {
+    config.title = 'pollr';
+
+    const landing = {
+      moduleId: './landing/landing',
+      name: 'landing',
+      nav: true,
+      route: ['', 'landing'],
+      title: 'landing'
+    };
+
+    const user = {
+      moduleId: './user/user',
+      name: 'user',
+      nav: true,
+      route: 'user',
+      title: 'user'
+    };
+
+    config.map([landing, user]);
+
+    this.router = router;
+  }
 };
