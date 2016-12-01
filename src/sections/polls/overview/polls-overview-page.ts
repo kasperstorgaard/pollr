b80535406aa34c1372dfe666d7a94e4cfc827daf
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-01 16:12:36 +0100

    feat(poll-overview-page): add polls data

--- src/sections/polls/overview/polls-overview-page.ts
+++ src/sections/polls/overview/polls-overview-page.ts
@@ -1,3 +1,26 @@
-export class Overview {
+import { inject } from 'aurelia-framework';
+import { collection } from '../../../shared/data/polls';
 
+@inject(collection)
+export class Overview {
+    public polls: Array<any>; // TODO: change to Array<Poll>
+
+    constructor (collection) {
+        this.setupSubscription(collection);
+    }
+
+    private updateData (polls) {
+        console.log(polls);
+
+        if (!polls) {
+            this.polls = [];
+            return;
+        }
+        this.polls = polls;
+    }
+
+    private setupSubscription (collection) {
+        collection.fetch().defaultIfEmpty()
+            .subscribe(this.updateData.bind(this));
+    }
 };
