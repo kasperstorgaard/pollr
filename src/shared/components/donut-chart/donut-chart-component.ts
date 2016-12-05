ab65a15c89d295ad5ed65e0b58bc829f57a2f22f
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-05 18:27:08 +0100

    refactor(donut-chart): renames/size by el

--- src/shared/components/donut-chart/donut-chart-component.ts
+++ src/shared/components/donut-chart/donut-chart-component.ts
@@ -10,48 +10,51 @@ export class DonutChartComponent {
     private arc;
     private pie;
     private svg;
-    private config;
     private path;
+    private size;
+    private color: Function;
 
     constructor (d3, element) {
         this.d3 = d3;
         this.element = element;
+        this.color = d3.scaleOrdinal(d3.schemeCategory20);
     }
 
+    // - lifecycle events - //
+    public attached () {
+        const rect = this.element.getBoundingClientRect();
+        this.size = Math.floor(rect.width);
+    }
+
+    // - change observers - //
     public chartDataChanged (data = []) {
         if (!data.length) {
             return;
         }
+        this.update(data);
+    }
+
+    // - private methods - //
+    private update (data) {
+        if (!this.size) {
+            return;
+        }
 
         if (!this.svg) {
-            this.config = this.getConfig(this.element);
-            this.createElements(this.element, this.config);
-            this.draw(this.svg, data, this.config);
+            this.createElements(this.element, this.size);
+            this.draw(this.svg, data, this.color);
             return;
         }
 
         this.reset(this.svg, data);
-        this.draw(this.svg, data, this.config);
+        this.draw(this.svg, data, this.color);
     }
 
-
-    private getConfig (element) {
-        const width = 300;
-        const height = 200;
-        const radius = Math.min(width, height) / 2;
-        const color = d3.scaleOrdinal(d3.schemeCategory20);
-        const transition = this.d3.transition()
-            .duration(750)
-            .ease(d3.easeElasticIn);
-
-        return { width, height, radius, color, transition };
-    }
-
-    private createElements (element: any, config: any) {
-        const { radius, width, height } = config;
+    private createElements (element: any, size: any) {
+        const radius = size / 2;
         this.arc = this.d3.arc()
-            .outerRadius(radius - 10)
-            .innerRadius(radius - 70);
+            .outerRadius(radius)
+            .innerRadius(radius * 0.6);
 
         this.pie = this.d3.pie()
             .value((d: any) => d.count)
@@ -59,16 +62,13 @@ export class DonutChartComponent {
             .padAngle(0.05);
 
         this.svg = this.d3.select(element).append('svg')
-            .attr('width', width)
-            .attr('height', height)
+            .attr('width', size)
+            .attr('height', size)
             .append('g')
-            .attr('transform',
-                `translate(${width / 2},${height / 2})`);
+            .attr('transform', `translate(${radius},${radius})`);
     }
 
-    private draw (svg, data, config) {
-        const { color } = config;
-
+    private draw (svg, data, color) {
         this.path = svg.datum(data).selectAll('path')
             .data(this.pie)
             .enter()
