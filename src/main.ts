916f386f1e70f2b164bf674f42406fd3b1202dc6
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-05 18:23:04 +0100

    chore(app/main): use mdl plugin

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { Aurelia } from 'aurelia-framework';
+import 'material-design-lite/material';
 
 export function configure(aurelia: Aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging();
+    .developmentLogging()
+    .plugin('aurelia-mdl-plugin');
 
   aurelia.start()
     .then(() => aurelia.setRoot('./src/app'));
