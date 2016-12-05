ec39e9159bb0bb111f664e93bfc016a64b852800
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-05 18:21:38 +0100

    chore(package): add material-design-lite - cleanup built files

--- jspm.config.js
+++ jspm.config.js
@@ -8,32 +8,7 @@ SystemJS.config({
   },
   devConfig: {
     "map": {
-      "ts": "github:frankwallis/plugin-typescript@5.3.1",
-      "typescript": "npm:typescript@2.1.1",
-      "net": "npm:jspm-nodelibs-net@0.2.0",
-      "module": "npm:jspm-nodelibs-module@0.2.0",
-      "child_process": "npm:jspm-nodelibs-child_process@0.2.0",
-      "plugin-typescript": "github:frankwallis/plugin-typescript@5.3.1"
-    },
-    "packages": {
-      "github:frankwallis/plugin-typescript@5.3.1": {
-        "map": {
-          "typescript": "npm:typescript@2.1.1"
-        }
-      },
-      "npm:source-map-support@0.4.6": {
-        "map": {
-          "source-map": "npm:source-map@0.5.6"
-        }
-      },
-      "npm:source-map@0.5.6": {
-        "map": {}
-      },
-      "npm:typescript@2.1.1": {
-        "map": {
-          "source-map-support": "npm:source-map-support@0.4.6"
-        }
-      }
+      "ts": "github:frankwallis/plugin-typescript@5.3.1"
     }
   },
   transpiler: "plugin-typescript",
@@ -49,6 +24,9 @@ SystemJS.config({
         }
       }
     }
+  },
+  map: {
+    "hz": "npm:@horizon/client@2.0.0"
   }
 });
 
@@ -59,6 +37,10 @@ SystemJS.config({
     "github:*/*.json"
   ],
   map: {
+    "aurelia-mdl-plugin": "npm:aurelia-mdl-plugin@0.1.16",
+    "d3-selection": "npm:d3-selection@1.0.3",
+    "d3": "npm:d3@4.4.0",
+    "d3-svg": "npm:d3-svg@0.2.0",
     "@horizon/client": "npm:@horizon/client@2.0.0",
     "assert": "npm:jspm-nodelibs-assert@0.2.0",
     "aurelia-binding": "npm:aurelia-binding@1.0.9",
@@ -85,21 +67,25 @@ SystemJS.config({
     "aurelia-templating-resources": "npm:aurelia-templating-resources@1.1.1",
     "aurelia-templating-router": "npm:aurelia-templating-router@1.0.0",
     "buffer": "npm:jspm-nodelibs-buffer@0.2.1",
+    "child_process": "npm:jspm-nodelibs-child_process@0.2.0",
     "constants": "npm:jspm-nodelibs-constants@0.2.0",
     "crypto": "npm:jspm-nodelibs-crypto@0.2.0",
     "css": "github:systemjs/plugin-css@0.1.32",
     "events": "npm:jspm-nodelibs-events@0.2.0",
     "fs": "npm:jspm-nodelibs-fs@0.2.0",
-    "google/material-design-lite": "github:google/material-design-lite@1.2.1",
     "http": "npm:jspm-nodelibs-http@0.2.0",
     "https": "npm:jspm-nodelibs-https@0.2.1",
-    "hz": "npm:@horizon/client@2.0.0",
+    "material-design-lite": "github:google/material-design-lite@1.2.1",
+    "module": "npm:jspm-nodelibs-module@0.2.0",
+    "net": "npm:jspm-nodelibs-net@0.2.0",
     "os": "npm:jspm-nodelibs-os@0.2.0",
     "path": "npm:jspm-nodelibs-path@0.2.1",
+    "plugin-typescript": "github:frankwallis/plugin-typescript@5.3.1",
     "process": "npm:jspm-nodelibs-process@0.2.0",
     "stream": "npm:jspm-nodelibs-stream@0.2.0",
     "string_decoder": "npm:jspm-nodelibs-string_decoder@0.2.0",
     "tls": "npm:jspm-nodelibs-tls@0.2.0",
+    "typescript": "npm:typescript@2.1.1",
     "url": "npm:jspm-nodelibs-url@0.2.0",
     "util": "npm:jspm-nodelibs-util@0.2.1",
     "vm": "npm:jspm-nodelibs-vm@0.2.0",
@@ -109,7 +95,7 @@ SystemJS.config({
     "npm:asn1.js@4.9.0": {
       "map": {
         "bn.js": "npm:bn.js@4.11.6",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
       }
     },
@@ -274,7 +260,7 @@ SystemJS.config({
         "cipher-base": "npm:cipher-base@1.0.3",
         "create-hash": "npm:create-hash@1.1.2",
         "evp_bytestokey": "npm:evp_bytestokey@1.0.0",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "systemjs-json": "github:systemjs/plugin-json@0.1.2"
       }
     },
@@ -289,7 +275,7 @@ SystemJS.config({
       "map": {
         "cipher-base": "npm:cipher-base@1.0.3",
         "des.js": "npm:des.js@1.0.0",
-        "inherits": "npm:inherits@2.0.1"
+        "inherits": "npm:inherits@2.0.3"
       }
     },
     "npm:browserify-rsa@4.0.1": {
@@ -305,7 +291,7 @@ SystemJS.config({
         "create-hash": "npm:create-hash@1.1.2",
         "create-hmac": "npm:create-hmac@1.1.4",
         "elliptic": "npm:elliptic@6.3.2",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "parse-asn1": "npm:parse-asn1@5.0.0"
       }
     },
@@ -316,7 +302,7 @@ SystemJS.config({
     },
     "npm:cipher-base@1.0.3": {
       "map": {
-        "inherits": "npm:inherits@2.0.1"
+        "inherits": "npm:inherits@2.0.3"
       }
     },
     "npm:core-util-is@1.0.2": {
@@ -331,7 +317,7 @@ SystemJS.config({
     "npm:create-hash@1.1.2": {
       "map": {
         "cipher-base": "npm:cipher-base@1.0.3",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "ripemd160": "npm:ripemd160@1.0.1",
         "sha.js": "npm:sha.js@2.4.8"
       }
@@ -339,7 +325,7 @@ SystemJS.config({
     "npm:create-hmac@1.1.4": {
       "map": {
         "create-hash": "npm:create-hash@1.1.2",
-        "inherits": "npm:inherits@2.0.1"
+        "inherits": "npm:inherits@2.0.3"
       }
     },
     "npm:crypto-browserify@3.11.0": {
@@ -350,7 +336,7 @@ SystemJS.config({
         "create-hash": "npm:create-hash@1.1.2",
         "create-hmac": "npm:create-hmac@1.1.4",
         "diffie-hellman": "npm:diffie-hellman@5.0.2",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "pbkdf2": "npm:pbkdf2@3.0.9",
         "public-encrypt": "npm:public-encrypt@4.0.0",
         "randombytes": "npm:randombytes@2.0.3"
@@ -358,7 +344,7 @@ SystemJS.config({
     },
     "npm:des.js@1.0.0": {
       "map": {
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "minimalistic-assert": "npm:minimalistic-assert@1.0.0"
       }
     },
@@ -375,7 +361,7 @@ SystemJS.config({
         "bn.js": "npm:bn.js@4.11.6",
         "brorand": "npm:brorand@1.0.6",
         "hash.js": "npm:hash.js@1.0.3",
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "systemjs-json": "github:systemjs/plugin-json@0.1.2"
       }
     },
@@ -386,11 +372,8 @@ SystemJS.config({
     },
     "npm:hash.js@1.0.3": {
       "map": {
-        "inherits": "npm:inherits@2.0.1"
+        "inherits": "npm:inherits@2.0.3"
       }
-    },
-    "npm:inherits@2.0.1": {
-      "map": {}
     },
     "npm:miller-rabin@4.0.0": {
       "map": {
@@ -433,7 +416,7 @@ SystemJS.config({
     },
     "npm:sha.js@2.4.8": {
       "map": {
-        "inherits": "npm:inherits@2.0.1"
+        "inherits": "npm:inherits@2.0.3"
       }
     },
     "npm:string_decoder@0.10.31": {
@@ -461,7 +444,7 @@ SystemJS.config({
     },
     "npm:stream-http@2.5.0": {
       "map": {
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "readable-stream": "npm:readable-stream@2.2.2",
         "builtin-status-codes": "npm:builtin-status-codes@2.0.0",
         "to-arraybuffer": "npm:to-arraybuffer@1.0.1",
@@ -476,7 +459,7 @@ SystemJS.config({
     },
     "npm:readable-stream@2.2.2": {
       "map": {
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "string_decoder": "npm:string_decoder@0.10.31",
         "core-util-is": "npm:core-util-is@1.0.2",
         "isarray": "npm:isarray@1.0.0",
@@ -571,7 +554,7 @@ SystemJS.config({
     },
     "npm:stream-browserify@2.0.1": {
       "map": {
-        "inherits": "npm:inherits@2.0.1",
+        "inherits": "npm:inherits@2.0.3",
         "readable-stream": "npm:readable-stream@2.2.2"
       }
     },
@@ -580,6 +563,162 @@ SystemJS.config({
         "isarray": "npm:isarray@1.0.0",
         "ieee754": "npm:ieee754@1.1.8",
         "base64-js": "npm:base64-js@1.2.0"
+      }
+    },
+    "github:frankwallis/plugin-typescript@5.3.1": {
+      "map": {
+        "typescript": "npm:typescript@2.1.1"
+      }
+    },
+    "npm:source-map-support@0.4.6": {
+      "map": {
+        "source-map": "npm:source-map@0.5.6"
+      }
+    },
+    "npm:source-map@0.5.6": {
+      "map": {}
+    },
+    "npm:typescript@2.1.1": {
+      "map": {
+        "source-map-support": "npm:source-map-support@0.4.6"
+      }
+    },
+    "npm:d3-svg@0.2.0": {
+      "map": {
+        "d3-selection": "npm:d3-selection@0.6.12"
+      }
+    },
+    "npm:d3@4.4.0": {
+      "map": {
+        "d3-polygon": "npm:d3-polygon@1.0.2",
+        "d3-format": "npm:d3-format@1.0.2",
+        "d3-geo": "npm:d3-geo@1.4.0",
+        "d3-axis": "npm:d3-axis@1.0.4",
+        "d3-drag": "npm:d3-drag@1.0.2",
+        "d3-zoom": "npm:d3-zoom@1.1.0",
+        "d3-path": "npm:d3-path@1.0.3",
+        "d3-scale": "npm:d3-scale@1.0.4",
+        "d3-chord": "npm:d3-chord@1.0.3",
+        "d3-time": "npm:d3-time@1.0.4",
+        "d3-queue": "npm:d3-queue@3.0.3",
+        "d3-force": "npm:d3-force@1.0.4",
+        "d3-color": "npm:d3-color@1.0.2",
+        "d3-shape": "npm:d3-shape@1.0.4",
+        "d3-interpolate": "npm:d3-interpolate@1.1.2",
+        "d3-collection": "npm:d3-collection@1.0.2",
+        "d3-array": "npm:d3-array@1.0.2",
+        "d3-transition": "npm:d3-transition@1.0.3",
+        "d3-quadtree": "npm:d3-quadtree@1.0.2",
+        "d3-voronoi": "npm:d3-voronoi@1.1.0",
+        "d3-time-format": "npm:d3-time-format@2.0.3",
+        "d3-brush": "npm:d3-brush@1.0.3",
+        "d3-dsv": "npm:d3-dsv@1.0.3",
+        "d3-timer": "npm:d3-timer@1.0.3",
+        "d3-ease": "npm:d3-ease@1.0.2",
+        "d3-hierarchy": "npm:d3-hierarchy@1.0.3",
+        "d3-request": "npm:d3-request@1.0.3",
+        "d3-selection": "npm:d3-selection@1.0.3",
+        "d3-random": "npm:d3-random@1.0.2",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2"
+      }
+    },
+    "npm:d3-zoom@1.1.0": {
+      "map": {
+        "d3-drag": "npm:d3-drag@1.0.2",
+        "d3-interpolate": "npm:d3-interpolate@1.1.2",
+        "d3-transition": "npm:d3-transition@1.0.3",
+        "d3-selection": "npm:d3-selection@1.0.3",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2"
+      }
+    },
+    "npm:d3-geo@1.4.0": {
+      "map": {
+        "d3-array": "npm:d3-array@1.0.2"
+      }
+    },
+    "npm:d3-drag@1.0.2": {
+      "map": {
+        "d3-selection": "npm:d3-selection@1.0.3",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2"
+      }
+    },
+    "npm:d3-scale@1.0.4": {
+      "map": {
+        "d3-format": "npm:d3-format@1.0.2",
+        "d3-array": "npm:d3-array@1.0.2",
+        "d3-collection": "npm:d3-collection@1.0.2",
+        "d3-color": "npm:d3-color@1.0.2",
+        "d3-interpolate": "npm:d3-interpolate@1.1.2",
+        "d3-time": "npm:d3-time@1.0.4",
+        "d3-time-format": "npm:d3-time-format@2.0.3"
+      }
+    },
+    "npm:d3-chord@1.0.3": {
+      "map": {
+        "d3-path": "npm:d3-path@1.0.3",
+        "d3-array": "npm:d3-array@1.0.2"
+      }
+    },
+    "npm:d3-shape@1.0.4": {
+      "map": {
+        "d3-path": "npm:d3-path@1.0.3"
+      }
+    },
+    "npm:d3-force@1.0.4": {
+      "map": {
+        "d3-collection": "npm:d3-collection@1.0.2",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2",
+        "d3-quadtree": "npm:d3-quadtree@1.0.2",
+        "d3-timer": "npm:d3-timer@1.0.3"
+      }
+    },
+    "npm:d3-interpolate@1.1.2": {
+      "map": {
+        "d3-color": "npm:d3-color@1.0.2"
+      }
+    },
+    "npm:d3-transition@1.0.3": {
+      "map": {
+        "d3-color": "npm:d3-color@1.0.2",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2",
+        "d3-ease": "npm:d3-ease@1.0.2",
+        "d3-interpolate": "npm:d3-interpolate@1.1.2",
+        "d3-selection": "npm:d3-selection@1.0.3",
+        "d3-timer": "npm:d3-timer@1.0.3"
+      }
+    },
+    "npm:d3-brush@1.0.3": {
+      "map": {
+        "d3-dispatch": "npm:d3-dispatch@1.0.2",
+        "d3-drag": "npm:d3-drag@1.0.2",
+        "d3-interpolate": "npm:d3-interpolate@1.1.2",
+        "d3-selection": "npm:d3-selection@1.0.3",
+        "d3-transition": "npm:d3-transition@1.0.3"
+      }
+    },
+    "npm:d3-request@1.0.3": {
+      "map": {
+        "d3-collection": "npm:d3-collection@1.0.2",
+        "d3-dispatch": "npm:d3-dispatch@1.0.2",
+        "d3-dsv": "npm:d3-dsv@1.0.3",
+        "xmlhttprequest": "npm:xmlhttprequest@1.8.0"
+      }
+    },
+    "npm:d3-time-format@2.0.3": {
+      "map": {
+        "d3-time": "npm:d3-time@1.0.4"
+      }
+    },
+    "npm:d3-dsv@1.0.3": {
+      "map": {
+        "iconv-lite": "npm:iconv-lite@0.4.15",
+        "commander": "npm:commander@2.9.0",
+        "rw": "npm:rw@1.3.2"
+      }
+    },
+    "npm:commander@2.9.0": {
+      "map": {
+        "graceful-readlink": "npm:graceful-readlink@1.0.1"
       }
     }
   }
