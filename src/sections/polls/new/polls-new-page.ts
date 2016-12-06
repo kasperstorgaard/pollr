2e57f56444719d116369b4925cc5656287d35005
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-06 08:38:06 +0100

    feat(polls-new): added form, actions

--- src/sections/polls/new/polls-new-page.ts
+++ src/sections/polls/new/polls-new-page.ts
@@ -1,3 +1,31 @@
-export class New {
+import { inject } from 'aurelia-framework';
+import { collection } from '../../../shared/data/polls';
+import { colors } from '../../../shared/config/colors';
 
+@inject(collection, colors)
+export class New {
+    public data: any[] = [];
+    public name: string = '';
+    public color: Function;
+    public optionName: string;
+
+    private collection: any;
+
+    constructor (collection, colors) {
+        this.collection = collection;
+        this.color = colors.standard;
+    }
+
+    public addOption (name) {
+        this.data = this.data.concat([{
+            id: encodeURI(name),
+            name,
+            value: 1
+        }]);
+        this.optionName = '';
+    }
+
+    public create (name, data) {
+        this.collection.store({ name, data });
+    }
 };
