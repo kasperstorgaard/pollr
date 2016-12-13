04e507f11bef75ee9d35ceaaa97a7c64992e6093
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-13 09:29:40 +0100

    refactor(polls-overview): mutate based on raw changes

--- src/sections/polls/overview/polls-overview-page.ts
+++ src/sections/polls/overview/polls-overview-page.ts
@@ -5,24 +5,63 @@ import { PollsStore } from '../../../shared/data/polls/polls-store';
 @inject(PollsStore, EventAggregator)
 export class Overview {
     public polls: any[] = []; // TODO: change to Array<Poll>
-    private subscriber: any;
+    private subscribers: any[];
     private ea: EventAggregator;
 
     constructor (store: PollsStore, ea: EventAggregator) {
-        this.subscriber = this.subscribe(store.collection);
+        const changesStream = this.getChangeStream(store.collection);
         this.ea = ea;
+        this.subscribers = this.subscribe(changesStream);
     }
 
     public unbind () {
-        this.subscriber.unsubscribe();
+        this.subscribers.forEach(s => s.unsubscribe());
     }
 
-    private update (polls) {
-        this.polls = polls;
+    private addPoll (poll) {
+        this.polls.push(poll);
     }
 
-    private subscribe (collection) {
-        return collection.watch()
-            .subscribe(this.update.bind(this));
+    private removePoll (poll) {
+        const idx = this.polls.findIndex(p => p.id === poll.id);
+        if (!idx) {
+            return;
+        }
+        this.polls.splice(idx, 1);
+    }
+
+    private updatePoll (poll) {
+        const existing = this.polls.find(p => p.id === poll.id);
+        if (!existing) {
+            return;
+        }
+        Object.assign(existing, poll);
+    }
+
+    private subscribe (changesStream) {
+        const added = changesStream
+            .defaultIfEmpty()
+            .filter(c => !c.old_val && c.new_val)
+            .map(c => c.new_val);
+
+        const removed = changesStream
+            .defaultIfEmpty()
+            .filter(c => c.new_val && !c.old_val)
+            .map(c => c.new_val);
+
+        const updated = changesStream
+            .defaultIfEmpty()
+            .filter(c => c.old_val && c.new_val)
+            .map(c => c.new_val);
+
+        return [
+            added.subscribe(this.addPoll.bind(this)),
+            removed.subscribe(this.removePoll.bind(this)),
+            updated.subscribe(this.updatePoll.bind(this))
+        ];
+    }
+
+    private getChangeStream (collection) {
+        return collection.watch({ rawChanges: true });
     }
 };
