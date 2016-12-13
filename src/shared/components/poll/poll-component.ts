1c6e374ca8de45e2d347307b86dcea37c506e9a8
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-13 08:56:41 +0100

    refactor(poll-component): take poll, not id - remove subscription

--- src/shared/components/poll/poll-component.ts
+++ src/shared/components/poll/poll-component.ts
@@ -1,71 +1,33 @@
 import { inject, bindable } from 'aurelia-framework';
-import { collection } from '../../data/polls';
 import { colors } from '../../config/colors';
+import { EventAggregator } from 'aurelia-event-aggregator';
 
-@inject(collection, colors)
+@inject(colors, EventAggregator)
 export class PollComponent {
-    @bindable public pollId: String;
-    public data: any[];
-    public name: String;
+    @bindable public poll: Object;
     public color: Function;
 
-    private poll: any;
-    private collection: any;
-    private subscriber: any;
+    private ea: EventAggregator;
 
-    constructor (collection, colors) {
-        this.collection = collection;
+    constructor (colors, ea: EventAggregator) {
         this.color = colors.standard;
-    }
-
-    // - lifecycle callbacks - //
-    public bind () {
-        this.poll = this.collection.find({ id: this.pollId });
-        this.subscriber = this.setupSubscription(this.poll);
-    }
-
-    public unbind () {
-        if (!this.subscriber) {
-            return;
-        }
-        this.subscriber.unsubscribe();
+        this.ea = ea;
     }
 
     // - public methods - //
-    public vote (pollId, id, data) {
-        const option = data.find(c => c.id === id);
-        option.value++;
-        this.storeData(pollId, data);
+    public vote (poll, optionId) {
+        this.ea.publish('POLL_VOTE', { poll, optionId });
     }
 
-    public reset (id, data) {
-        data.forEach(c => c.value = 1);
-        this.storeData(id, data);
+    public reset (poll) {
+        this.ea.publish('POLL_RESET', { poll });
     }
 
-    public remove (id) {
-        this.collection.remove(id);
+    public remove (poll) {
+        this.ea.publish('POLL_REMOVE', { poll });
     }
 
     public getBgStyle (id) {
         return `color: ${this.color(id)}`;
-    }
-
-    // - private methods - //
-    private dataUpdated (poll) {
-        if (!poll) {
-            return;
-        }
-        const { name, data } = poll;
-        Object.assign(this, { name, data });
-    }
-
-    private storeData (id, data) {
-        this.collection.update({ id,  data });
-    }
-
-    private setupSubscription (poll) {
-        return poll.watch().defaultIfEmpty()
-            .subscribe(this.dataUpdated.bind(this));
     }
 }
