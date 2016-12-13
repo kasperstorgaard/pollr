1ea8c7909ae0624048964db0a8fcded61c19b0a8
kasper storgaard <kasper.storgaard@falcon.io>
2016-12-13 08:54:44 +0100

    refactor(polls-new): use pollsStore - rename data => options - import pollsStore

--- src/sections/polls/new/polls-new-page.ts
+++ src/sections/polls/new/polls-new-page.ts
@@ -1,23 +1,26 @@
 import { inject } from 'aurelia-framework';
-import { collection } from '../../../shared/data/polls';
+import { EventAggregator } from 'aurelia-event-aggregator';
+import { PollsStore } from '../../../shared/data/polls/polls-store';
 import { colors } from '../../../shared/config/colors';
 
-@inject(collection, colors)
+@inject(PollsStore, EventAggregator, colors)
 export class New {
-    public data: any[] = [];
+    public options: any[] = [];
     public name: string = '';
     public color: Function;
     public optionName: string;
 
     private collection: any;
+    private ea: EventAggregator;
 
-    constructor (collection, colors) {
-        this.collection = collection;
+    constructor (store: PollsStore, ea: EventAggregator, colors) {
+        this.collection = store.collection;
+        this.ea = ea;
         this.color = colors.standard;
     }
 
     public addOption (name) {
-        this.data = this.data.concat([{
+        this.options = this.options.concat([{
             id: encodeURI(name),
             name,
             value: 1
@@ -25,7 +28,8 @@ export class New {
         this.optionName = '';
     }
 
-    public create (name, data) {
-        this.collection.store({ name, data });
+    public create (name, options) {
+        const poll = { name, options };
+        this.ea.publish('POLL_ADD', { poll });
     }
 };
